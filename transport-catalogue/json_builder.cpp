251980ab652228acd557010e7bb5ed9c0963a827
Solodkov Alexander <solodkov.alexander.ru@gmail.com>
2024-06-15 19:48:21 +0300

    SPRINT-11 Переделан json::Builder по-другому (через наследование и удаление лишних методов, а также перегрузку методов (DictKeyValueContext.Value, ArrayContext.Value))

--- transport-catalogue/json_builder.cpp
+++ transport-catalogue/json_builder.cpp
@@ -6,7 +6,43 @@ Builder::Builder() {
     nodes_stack_.emplace_back(&root_);
 }
 
-Builder& Builder::Key(std::string dict_key) {
+Builder::DictContext Builder::StartDict() {
+    // Если строка последн. элемент (значит перед нами были StartDict, Key) или если у нас массив
+    if (GetLastNode()->IsString() || GetLastNode()->IsArray()) {
+        auto dict_node = new Node(Dict{});
+        nodes_stack_.push_back(dict_node);
+    } else if (nodes_stack_.size() == 1) { 
+        // Если размер стэка всего один: устанавливаем значение (дальше Key, EndDict)
+        Node::Value& last_node_value = GetLastNode()->GetValue();
+        last_node_value = std::move(Dict{});
+    } else {
+        throw std::logic_error("Builder.StartDict: Failed: try to start dict to the wrong place");
+    }
+    
+    return Builder::DictContext{*this};
+}
+
+Builder::MainContext Builder::EndDict() {
+    if (GetLastNode()->IsDict()) {
+        if (nodes_stack_.size() == 1) {
+            nodes_stack_.pop_back();
+        } else {
+            // Получаем наш законченный словарь
+            auto dict_node = GetLastNode();
+            nodes_stack_.pop_back();
+
+            // Завершаем в словаре ключ-значение или добавляем в массив элемент
+            Value(dict_node->GetValue());
+            delete dict_node;
+        }
+    } else {
+        throw std::logic_error("Builder.EndDict: Failed: last node is not dict");
+    }
+
+    return Builder::MainContext{*this};
+}
+
+Builder::DictKeyValueContext Builder::Key(std::string dict_key) {
     // Если последн. элемент словарь: добавляем ключ словаря как последн. элемент (дальше Value, StartArray, StartDict)
     if (GetLastNode()->IsDict()) {
         auto key_node = new Node{std::move(dict_key)};
@@ -15,10 +51,44 @@ Builder& Builder::Key(std::string dict_key) {
         throw std::logic_error("Builder.Key: Failed: last node is not dict");
     }
     
-    return *this;
+    return Builder::DictKeyValueContext{*this};
 }
 
-Builder& Builder::Value(Node::Value value) {
+Builder::ArrayContext Builder::StartArray() {
+    // Если строка последн. элемент (значит перед нами были StartDict, Key) или если у нас массив
+    if (GetLastNode()->IsString() || GetLastNode()->IsArray()) {
+        auto dict_node = new Node(Array{});
+        nodes_stack_.push_back(dict_node);
+    } else if (nodes_stack_.size() == 1) { 
+        // Если размер стэка всего один: устанавливаем значение (дальше Value, StartDict, EndArray)
+        GetLastNode()->GetValue().emplace<Array>(Array{});
+    } else {
+        throw std::logic_error("Builder.StartArray: Failed: try to start dict to the wrong place");
+    }
+    
+    return Builder::ArrayContext{*this};
+}
+
+Builder::MainContext Builder::EndArray() {
+    if (GetLastNode()->IsArray()) {
+        if (nodes_stack_.size() == 1) {
+            nodes_stack_.pop_back();
+        } else {
+            // Получаем наш массив
+            auto array_node = GetLastNode();
+            nodes_stack_.pop_back();
+
+            // Завершаем в словаре ключ-значение или добавляем в след. массив элемент
+            Value(array_node->GetValue());
+            delete array_node;
+        }
+    } else {
+        throw std::logic_error("Builder.EndDict: Failed: last node is not dict");
+    }
+    return Builder::MainContext{*this};
+}
+
+Builder::MainContext Builder::Value(Node::Value value) {
     // Если строка последн. элемент: значит перед нами были StartDict, Key
     if (GetLastNode()->IsString()) {
         // Забираем ключ
@@ -41,77 +111,7 @@ Builder& Builder::Value(Node::Value value) {
         throw std::logic_error("Builder.Value: Failed: try to set value to the wrong place");
     }
     
-    return *this;
-}
-
-Builder::DictItemContext Builder::StartDict() {
-    // Если строка последн. элемент (значит перед нами были StartDict, Key) или если у нас массив
-    if (GetLastNode()->IsString() || GetLastNode()->IsArray()) {
-        auto dict_node = new Node(Dict{});
-        nodes_stack_.push_back(dict_node);
-    } else if (nodes_stack_.size() == 1) { 
-        // Если размер стэка всего один: устанавливаем значение (дальше Key, EndDict)
-        Node::Value& last_node_value = GetLastNode()->GetValue();
-        last_node_value = std::move(Dict{});
-    } else {
-        throw std::logic_error("Builder.StartDict: Failed: try to start dict to the wrong place");
-    }
-    
-    return Builder::DictItemContext{*this};
-}
-
-Builder::ArrayItemContext Builder::StartArray() {
-    // Если строка последн. элемент (значит перед нами были StartDict, Key) или если у нас массив
-    if (GetLastNode()->IsString() || GetLastNode()->IsArray()) {
-        auto dict_node = new Node(Array{});
-        nodes_stack_.push_back(dict_node);
-    } else if (nodes_stack_.size() == 1) { 
-        // Если размер стэка всего один: устанавливаем значение (дальше Value, StartDict, EndArray)
-        GetLastNode()->GetValue().emplace<Array>(Array{});
-    } else {
-        throw std::logic_error("Builder.StartArray: Failed: try to start dict to the wrong place");
-    }
-    
-    return Builder::ArrayItemContext{*this};
-}
-
-Builder& Builder::EndDict() {
-    if (GetLastNode()->IsDict()) {
-        if (nodes_stack_.size() == 1) {
-            nodes_stack_.pop_back();
-        } else {
-            // Получаем наш законченный словарь
-            auto dict_node = GetLastNode();
-            nodes_stack_.pop_back();
-
-            // Завершаем в словаре ключ-значение или добавляем в массив элемент
-            Value(dict_node->GetValue());
-            delete dict_node;
-        }
-    } else {
-        throw std::logic_error("Builder.EndDict: Failed: last node is not dict");
-    }
-
-    return *this;
-}
-
-Builder& Builder::EndArray() {
-    if (GetLastNode()->IsArray()) {
-        if (nodes_stack_.size() == 1) {
-            nodes_stack_.pop_back();
-        } else {
-            // Получаем наш массив
-            auto array_node = GetLastNode();
-            nodes_stack_.pop_back();
-
-            // Завершаем в словаре ключ-значение или добавляем в след. массив элемент
-            Value(array_node->GetValue());
-            delete array_node;
-        }
-    } else {
-        throw std::logic_error("Builder.EndDict: Failed: last node is not dict");
-    }
-    return *this;
+    return Builder::MainContext{*this};
 }
 
 Node Builder::Build() {
--- transport-catalogue/json_builder.h
+++ transport-catalogue/json_builder.h
@@ -7,61 +7,88 @@ using namespace std::literals;
 namespace json {
 
 class Builder {
+
+// Обозначаем, с какими классами (до их определения) будем работать
+private:
+    class DictContext;
+    class DictKeyValueContext;
+    class ArrayContext;
+
+private:
+    // Контекст JSON по умолчанию (реализованы все переходы делегированием Builder-у)
+    class MainContext {
+    public:
+        MainContext(Builder& builder) 
+            : builder_(builder) 
+        {}
+
+    public:
+        // Методы для работы только с словарем
+        DictContext StartDict() { return builder_.StartDict(); }
+        MainContext EndDict() { return builder_.EndDict(); }
+        DictKeyValueContext Key(std::string dict_key) { return builder_.Key(std::move(dict_key)); }
+
+        // Методы по работе только с массивом
+        ArrayContext StartArray() { return builder_.StartArray(); }
+        MainContext EndArray() { return builder_.EndArray(); }
+
+        // Метод по работе с константами/значением по ключу словаря/элементом массива
+        MainContext Value(Node::Value value) { return builder_.Value(std::move(value)); }
+        
+        // Метода получения заврешенного JSON объекта
+        Node Build() { return builder_.Build(); }
+
+    protected:
+        Builder& builder_;
+    };
+
+    // Контекст JSON словаря, т.е. после вызова метода StartDict
+    class DictContext : public MainContext {
+    public:
+        DictContext(MainContext base) : MainContext(base) {}
+        DictContext StartDict() = delete;
+        DictContext StartArray() = delete;
+        DictContext EndArray() = delete;
+        MainContext Value(Node::Value value) = delete;
+        Node Build() = delete;
+    };
+    // Контекст JSON значения по ключу словаря, т.е. после вызова метода Key в словаре (после вызова метода StartDict)
+    class DictKeyValueContext : public MainContext {
+    public:
+        DictKeyValueContext(MainContext base) : MainContext(base) {}
+        DictContext EndDict() = delete;
+        DictKeyValueContext Key(std::string dict_key) = delete;        
+        DictContext EndArray() = delete;
+        DictContext Value(Node::Value value) {
+            return MainContext::Value(std::move(value));
+        };
+        Node Build() = delete;
+    };
+    // Контекст JSON массива, т.е. после вызова метода StartArray
+    class ArrayContext : public MainContext {
+    public:
+        ArrayContext(MainContext base) : MainContext(base) {}
+        DictContext EndDict() = delete;
+        MainContext Key(std::string dict_key) = delete;
+        ArrayContext Value(Node::Value value) {
+            return MainContext::Value(std::move(value));
+        };
+        Node Build() = delete;
+    };
+
 public:
     Builder();
-    
-public: 
-    class DictItemContext {
-    public:
-        DictItemContext(Builder& builder) : builder_(builder) {}
-
-    public:
-        class DictItemKeyContext {
-        public:
-            DictItemKeyContext(Builder& builder) : builder_(builder) {}
-            DictItemContext Value(Node::Value value) { 
-                builder_.Value(std::move(value)); 
-                return DictItemContext{builder_};
-            }
-            auto StartDict() { return builder_.StartDict(); }
-            auto StartArray() { return builder_.StartArray(); }
-        protected:
-            Builder& builder_;
-        };
-
-        DictItemKeyContext Key(std::string dict_key) {
-            builder_.Key(std::move(dict_key));
-            return DictItemKeyContext{builder_};
-        }
-        Builder& EndDict() { return builder_.EndDict(); }
-        
-    private:
-        Builder& builder_;
-    };
-
-    class ArrayItemContext {
-    public:
-        ArrayItemContext(Builder& builder) : builder_(builder) {}
-
-    public:
-        ArrayItemContext Value(Node::Value value) { 
-            builder_.Value(std::move(value)); 
-            return ArrayItemContext{builder_};
-        }
-        auto StartDict() { return builder_.StartDict(); }   
-        auto StartArray() { return builder_.StartArray(); }
-        Builder& EndArray() { return builder_.EndArray(); }
-    private:
-        Builder& builder_;
-    };
 
 public:
-    Builder& Key(std::string dict_key);
-    Builder& Value(Node::Value value);
-    DictItemContext StartDict();    
-    ArrayItemContext StartArray();
-    Builder& EndDict();
-    Builder& EndArray();
+    DictContext StartDict();
+    MainContext EndDict();
+    DictKeyValueContext Key(std::string dict_key);
+     
+    ArrayContext StartArray();
+    MainContext EndArray();
+
+    MainContext Value(Node::Value value); 
+
     Node Build();
 
 private:
@@ -73,3 +100,56 @@ private:
 };
 
 }  // namespace json
+
+/**
+ * Tests compilation errors:
+ *  
+    // 1
+    json::Builder{}.StartDict().Key(""s).Value(""s);
+    json::Builder{}.StartDict().Key(""s).StartArray();
+    json::Builder{}.StartDict().Key(""s).StartDict();
+    json::Builder{}.StartDict().Key(""s).EndDict();
+    json::Builder{}.StartDict().Key(""s).EndArray();
+    json::Builder{}.StartDict().Key(""s).EndDict();
+
+    // 2
+    json::Builder{}.StartDict().Key(""s).Value(""s).Key("s");
+    json::Builder{}.StartDict().Key(""s).Value(""s).EndDict();
+    json::Builder{}.StartDict().Key(""s).Value(""s).Value(""s);
+    json::Builder{}.StartDict().Key(""s).Value(""s).StartArray();
+    json::Builder{}.StartDict().Key(""s).Value(""s).StartDict();
+    json::Builder{}.StartDict().Key(""s).Value(""s).EndArray();
+
+    // 3
+    json::Builder{}.StartDict().Key("s");
+    json::Builder{}.StartDict().EndDict();
+    json::Builder{}.StartDict().Value(""s);
+    json::Builder{}.StartDict().StartArray();
+    json::Builder{}.StartDict().StartDict();
+    json::Builder{}.StartDict().EndArray();
+
+    // 4
+    json::Builder{}.StartArray().Key("s");
+    json::Builder{}.StartArray().EndDict();
+    json::Builder{}.StartArray().Value(""s);
+    json::Builder{}.StartArray().StartArray();
+    json::Builder{}.StartArray().StartDict();
+    json::Builder{}.StartArray().EndArray();
+
+    // 5.1
+    json::Builder{}.StartArray().Value(""s).Key("s");
+    json::Builder{}.StartArray().Value(""s).EndDict();
+    json::Builder{}.StartArray().Value(""s).Value(""s);
+    json::Builder{}.StartArray().Value(""s).StartArray();
+    json::Builder{}.StartArray().Value(""s).StartDict();
+    json::Builder{}.StartArray().Value(""s).EndArray();
+    
+    // 5.2
+    json::Builder{}.StartArray().Value(""s).Value(""s).Key("s");
+    json::Builder{}.StartArray().Value(""s).Value(""s).EndDict();
+    json::Builder{}.StartArray().Value(""s).Value(""s).Value(""s);
+    json::Builder{}.StartArray().Value(""s).Value(""s).StartArray();
+    json::Builder{}.StartArray().Value(""s).Value(""s).StartDict();
+    json::Builder{}.StartArray().Value(""s).Value(""s).EndArray(); 
+ * 
+ */
